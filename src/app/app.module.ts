f77f69ede6af7eefc3d5ae38e868b621f1fb4527
toan Nguyen <toannq0988@gmail.com>
2018-07-05 23:47:50 +0700

    lesson10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MainComponent } from './main/main.component';
+import { FunctionComponent } from './main/function/function.component';
+import { HomeComponent } from './main/home/home.component';
+import { ProductComponent } from './main/product/product.component';
+import { ProductCategoryComponent } from './main/product-category/product-category.component';
+import { RoleComponent } from './main/role/role.component';
+import { UserComponent } from './main/user/user.component';
+import { LoginComponent } from 'src/app/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    FunctionComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductCategoryComponent,
+    RoleComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
