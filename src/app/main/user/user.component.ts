4eec6e1c7cb0bfb298b0dfc3f9521b531303d347
toan Nguyen <toannq0988@gmail.com>
2018-07-24 22:19:29 +0700

    Lesson24

--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataService } from '../../core/services/data.service';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import { NotificationService } from '../../core/services/notification.service';
+import { MessageContstants } from '../../core/common/message.constants';
 
+import { IMultiSelectOption } from 'angular2-dropdown-multiselect'
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -7,9 +12,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('modalAddEdit') public modalAddEdit: ModalDirective;
+  public myRoles: string[] = [];
+  public pageIndex: number = 1;
+  public pageSize: number = 20;
+  public pageDisplay: number = 10;
+  public totalRow: number;
+  public filter: string = '';
+  public users: any[];
+  public entity: any;
+
+  public allRoles: IMultiSelectOption[] = [];
+  public roles: any[];
+
+  public dateOptions: any = {
+    locale: { format: 'DD/MM/YYYY' },
+    alwaysShowCalendars: false,
+    singleDatePicker: true
+  };
+
+  constructor(private _dataService: DataService, private _notificationService: NotificationService) { }
 
   ngOnInit() {
+    this.loadRoles();
+    this.loadData();
   }
 
+  loadData() {
+    this._dataService.get('/api/appUser/getlistpaging?page=' + this.pageIndex + '&pageSize=' + this.pageSize + '&filter=' + this.filter)
+      .subscribe((response: any) => {
+        this.users = response.Items;
+        this.pageIndex = response.PageIndex;
+        this.pageSize = response.PageSize;
+        this.totalRow = response.TotalRows;
+      });
+  }
+  loadRoles() {
+    this._dataService.get('/api/appRole/getlistall').subscribe((response: any[]) => {
+      this.allRoles = [];
+      for (let role of response) {
+        this.allRoles.push({ id: role.Name, name: role.Description });
+      }
+    }, error => this._dataService.handleError(error));
+  }
+  loadUserDetail(id: any) {
+    this._dataService.get('/api/appUser/detail/' + id).subscribe((response: any) => {
+      this.entity = response;
+
+    })
+  }
+  showAddModal() {
+    this.entity = {};
+    this.modalAddEdit.show();
+  }
+  showEditModal(id: any) {
+    this.loadUserDetail(id);
+    this.modalAddEdit.show();
+  }
+  saveChange(valid: boolean) {
+    if (valid) {
+      if (this.entity.Id == undefined) {
+        this._dataService.post('/api/appUser/add', JSON.stringify(this.entity))
+          .subscribe((response: any) => {
+            this.loadData();
+            this.modalAddEdit.hide();
+            this._notificationService.printSuccessMessage(MessageContstants.CREATED_OK_MSG);
+          }, error => this._dataService.handleError(error));
+      }
+      else {
+        this._dataService.put('/api/appUser/update', JSON.stringify(this.entity))
+          .subscribe((response: any) => {
+            this.loadData();
+            this.modalAddEdit.hide();
+            this._notificationService.printSuccessMessage(MessageContstants.UPDATED_OK_MSG);
+          }, error => this._dataService.handleError(error));
+      }
+    }
+  }
+  public selectGender(event) {
+    this.entity.Gender = event.target.value
+  }
 }
--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -2,18 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { Routes, RouterModule } from '@angular/router';
-
+import { DataService } from '../../core/services/data.service';
+import { NotificationService } from '../../core/services/notification.service';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { MultiselectDropdownModule } from 'angular2-dropdown-multiselect'
+import { Daterangepicker } from 'ng2-daterangepicker';
 const userRoutes: Routes = [
-   //localhost:4200/main/user
+  //localhost:4200/main/user
   { path: '', redirectTo: 'index', pathMatch: 'full' },
-   //localhost:4200/main/user/index
+  //localhost:4200/main/user/index
   { path: 'index', component: UserComponent }
 ]
 @NgModule({
   imports: [
     CommonModule,
+    PaginationModule,
+    FormsModule,
+    MultiselectDropdownModule,
+    Daterangepicker,
+    ModalModule.forRoot(),
     RouterModule.forChild(userRoutes)
   ],
-  declarations: [UserComponent]
+  declarations: [UserComponent],
+  providers: [DataService, NotificationService]
 })
 export class UserModule { }
