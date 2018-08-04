16e42852b7a4aaeb82df51d9739456e28a3b0d20
toan Nguyen <toannq0988@gmail.com>
2018-08-04 09:28:34 +0700

    lesson24

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -7,8 +7,9 @@ import { UserModule } from './user/user.module';
 import { HomeModule } from './home/home.module';
 import { UtilityService } from '../core/services/utility.service';
 import { AuthenService } from '../core/services/authen.service';
-import { SidebarMenuComponent } from '../shared/sidebar-menu/sidebar-menu.component';
-import { TopMenuComponent } from '../Shared/top-menu/top-menu.component';
+import { SidebarMenuComponent } from './Shared/sidebar-menu/sidebar-menu.component';
+import { TopMenuComponent } from './Shared/top-menu/top-menu.component';
+
 
 @NgModule({
   imports: [
