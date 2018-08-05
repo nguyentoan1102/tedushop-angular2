5e19016a3a82a791b823708ee863558f80daa2d7
toan Nguyen <toannq0988@gmail.com>
2018-08-05 09:15:29 +0700

    lesson33

--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -12,7 +12,8 @@ export const mainRoutes: Routes = [
       //localhost:4200/main/user
       { path: 'user', loadChildren: './user/user.module#UserModule' },
       { path: 'role', loadChildren: './role/role.module#RoleModule' },
-      { path: 'function', loadChildren: './function/function.module#FunctionModule' }
+      { path: 'function', loadChildren: './function/function.module#FunctionModule' },
+      { path: 'product-category', loadChildren: './product-category/product-category.module#ProductCategoryModule' }
     ]
   }
 
